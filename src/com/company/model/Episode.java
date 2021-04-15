902c4d4e297e20b8085322e8e46f0743fb18901f
Joao Paulo <paulomedeiros5000@gmail.com>
2021-04-15 00:44:13 -0300

    Initial Commit

--- src/com/company/model/Episode.java
+++ src/com/company/model/Episode.java
@@ -5,5 +5,33 @@ public class Episode {
     private String title;
     private String duration;
 
+    public Episode(Season seasonN, String title, String duration) {
+        this.seasonN = seasonN;
+        this.title = title;
+        this.duration = duration;
+    }
 
+    public Season getSeasonN() {
+        return seasonN;
+    }
+
+    public void setSeasonN(Season seasonN) {
+        this.seasonN = seasonN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
 }
